e6d0c8f3c0e701ff3e916e30aecffff8a9a1a413
rajukr1998 <rajukumar.bih12@gmail.com>
2023-11-13 20:04:20 +0530

    Add Day3 datatypes

---
+++ Day3/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not brouser
+
+console.log(3 + 3); // Code readability should be high
+console.log("Raju")
+
+let name = "Raju"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+//Number => 2 to power 53
+//bigint 
+//string > ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "Raju");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
